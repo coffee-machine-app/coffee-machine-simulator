94369963ed29a4cd66d63b19f6f03b29fd01eaed
garnier5 <garnier21axel@gmail.com>
2020-10-12 12:02:30 +0200

    Page creation

--- App.vue
+++ App.vue
@@ -1,17 +1,36 @@
 <template>
-  <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
-  </view>
+  <safe-area-view class="container">
+    <view class="header">
+      <text class="title">Coffee</text>
+    </view>
+    <view class="body">
+      <text class="">Machine state</text>
+    </view>
+  </safe-area-view>
 </template>
+
+
+
+<script>
+export default {
+  components: {  },
+};
+</script>
+
 
 <style>
 .container {
-  background-color: white;
+  flex: 1;
+}
+.title {
+  font-size: 30;
+  margin: 5;
+  align-items: center;
+  text-align: center;
+}
+.body {
   align-items: center;
   justify-content: center;
   flex: 1;
-}
-.text-color-primary {
-  color: blue;
 }
 </style>
