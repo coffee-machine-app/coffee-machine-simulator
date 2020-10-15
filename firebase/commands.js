52d920a050ebaf41490d5bde2514f556bfbce2a7
garnier5 <garnier21axel@gmail.com>
2020-10-15 18:01:56 +0200

    Manage if the coffee machine stopped

--- firebase/commands.js
+++ firebase/commands.js
@@ -3,37 +3,52 @@ import { firebase } from './config.js'
 async function checkStatus(status) {
     //Search all the messages not from the user
     firebase.firestore().collection('status').orderBy("timestamp", "desc").limit(1).onSnapshot(async function (result) {;
-        //Check if there is changes
-        if (status == "wait"){
+        try{
+            var timestamp = Date.now();
+            var now = Date.now();
+            
+            //Check if there is changes
             result.docChanges().forEach(function (doc) {
                 //If the message is added
                 if (doc.type == 'added') {
                     status.length = 0;
                     status.push(doc.doc.data().status);
+                    timestamp = doc.doc.data().timestamp;
                 }
             });
+
+            //Manage if the machine stop working during making the coffee
+            if(status != "wait" && now > timestamp+60000){
+                sendStatus("wait");
+                status.length = 0;
+                status.push("wait");
+            }
+
+            if(status == "short"){
+                console.log("doing short coffee");
+                sendStatus("doing short coffee");
+                status.length = 0;
+                status.push("doing short coffee");
+                setTimeout(function () { 
+                    sendStatus("wait"); 
+                    status.length = 0;
+                    status.push("wait");
+                }, 5000);
+                
+            } else if(status == "long"){
+                console.log("doing long coffee");
+                sendStatus("doing long coffee");
+                status.length = 0;
+                status.push("doing long coffee");
+                setTimeout(function () { 
+                    sendStatus("wait"); 
+                    status.length = 0;
+                    status.push("wait");
+                }, 10000);
+            }
         }
-        if(status == "short"){
-            console.log("short");
-            sendStatus("doing short coffee");
-            status.length = 0;
-            status.push("doing short coffee");
-            setTimeout(function () { 
-                sendStatus("wait"); 
-                status.length = 0;
-                status.push("wait");
-            }, 5000);
-            
-        } else if(status == "long"){
-            console.log("long");
-            sendStatus("doing long coffee");
-            status.length = 0;
-            status.push("doing long coffee");
-            setTimeout(function () { 
-                sendStatus("wait"); 
-                status.length = 0;
-                status.push("wait");
-            }, 10000);
+        catch(e){
+            sendStatus("wait");
         }
     });
     return status;
@@ -42,7 +57,7 @@ async function checkStatus(status) {
 //Create a command in firestore
 async function sendStatus(status) {
     try {
-       console.log(status);
+       console.log("send this status :", status);
        var id = Date.now();
         await firebase.firestore().collection('status').doc().set({
             key: id,
